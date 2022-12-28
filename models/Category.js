51bd14e83fb2f80bb9e258a93b8cd200f885bc42
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-12-28 17:01:33 +0200

    added Product model

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,23 @@
+// Import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// Import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Category extends Model { }
 
+// Set up fields and rules for Product model
 Category.init(
   {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      allowNull : false, 
+      allowNull: false,
       autoIncrement: true
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull : false
+      allowNull: false
     }
   },
   {
