24639cadcb7ed7ece16eb3d9190ddcc739470999
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2022-12-29 14:45:06 +0200

    executed association methods and created the following relationships between them

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,6 @@
 const { Model, DataTypes } = require('sequelize');
 // Import our database connection from config.js
 const sequelize = require('../config/connection.js');
-
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Category extends Model { }
 
